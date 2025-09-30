f6f2a20440c0222a08723b4691c63d68de144a00
Cristiano3120 <Cristianocx7@gmail.com>
2025-09-30 23:27:09 +0200

    Fixes #8

--- CacxClient/Windows/CreateAccWindow.xaml.cs
+++ CacxClient/Windows/CreateAccWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CacxClient.Windows;
+﻿using System.Windows;
+
+namespace CacxClient.Windows;
 
 /// <summary>
 /// Interaction logic for CreateAccWindow.xaml
@@ -8,5 +10,8 @@ public partial class CreateAccWindow : BaseWindow
     public CreateAccWindow()
     {
         InitializeComponent();
+        GeneratePwBtn.Click += GeneratePwBtn_Click;
     }
+
+    private void GeneratePwBtn_Click(object sender, RoutedEventArgs e) => throw new NotImplementedException();
 }
